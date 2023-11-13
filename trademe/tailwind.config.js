730b2182697fee1016b09237bbb9067b1065be9e
tinhtt <tinhtrantrung.vn@gmail.com>
2023-11-13 15:03:26 +1300

    Complete trademe pay a fine.

--- trademe/tailwind.config.js
+++ trademe/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.html",
+  ],
   theme: {
     extend: {},
   },
