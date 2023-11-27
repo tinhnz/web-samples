a354bb1bf76cde7f5509ee96517e41fe1d67c74c
tinhtt <tinhtrantrung.vn@gmail.com>
2023-11-27 22:29:45 +1300

    Turing - implement headlines.

--- turing/src/components/Button.jsx
+++ turing/src/components/Button.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable react/prop-types */
-const Button = ({ size, children }) => {
+const Button = ({ size, children, className }) => {
   return size === 'large' ? (
-    <button className='border rounded-md py-4 px-6 font-medium'>{children}</button>
+    <button className={`border rounded-md py-4 px-6 font-medium ${className}`}>{children}</button>
   ) : (
-    <button className='border rounded-md py-3 px-6 font-medium text-sm'>{children}</button>
+    <button className={`border rounded-md py-3 px-6 font-medium text-sm ${className}`}>
+      {children}
+    </button>
   );
 };
 
