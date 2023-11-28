bdbb68b1854a7a6fb184ae4cb2d9a5d9f50fc789
tinhtt <tinhtrantrung.vn@gmail.com>
2023-11-28 16:54:05 +1300

    Turing - implement WorldStats component.

--- turing/src/App.jsx
+++ turing/src/App.jsx
@@ -7,6 +7,7 @@ import Hero from './components/Hero';
 import Banner from './components/Banner';
 import Headline from './components/Headline';
 import Stats from './components/Stats';
+import WorldStats from './components/WorldStats';
 
 const App = () => {
   return (
@@ -87,6 +88,30 @@ const App = () => {
       <Headline />
 
       <Stats />
+
+      <WorldStats />
+
+      {/* Promo 3 */}
+      <section className='py-16'>
+        <div className='container grid grid-cols-2 gap-32 items-center justify-center'>
+          <div className='order-2'>
+            <img src={promo3} alt='promo3' />
+          </div>
+          <div>
+            <p className='text-sm uppercase font-bold text-[#737373]'>For developers</p>
+            <h3 className='text-3xl font-medium mt-2 mb-4'>
+              Get AI-matched to high-paying U.S. tech jobs
+            </h3>
+            <p className='text-lg mb-8'>
+              Once you pass our vetting process, you’ll enjoy opportunities with top clients who
+              need your expert skills.
+            </p>
+            <div className='flex gap-4'>
+              <Button size='large'>Apply for Jobs</Button>
+            </div>
+          </div>
+        </div>
+      </section>
 
       <section className='py-16'></section>
     </>
---
+++ turing/src/components/WorldStats.jsx
@@ -0,0 +1,43 @@
+import Button from './Button';
+import world from '../assets/world.svg';
+
+const WorldStats = () => {
+  return (
+    <section className='py-16 bg-gradient-to-r from-[#4e61c2] to-[#3e85e2] relative'>
+      <div className='container'>
+        <div className='w-[55%] text-white relative z-10'>
+          <h5 className='text-5xl/[60px] font-extralight'>
+            3 million+ developers <br />
+            from 150 countries <br />
+            have already joined Turing
+          </h5>
+          <div className='flex gap-12 mt-8'>
+            <div>
+              <h6 className='text-5xl font-medium mb-3'>15+</h6>
+              <p className='text-xl'>Job types</p>
+            </div>
+            <div>
+              <h6 className='text-5xl font-medium mb-3'>100+</h6>
+              <p className='text-xl'>Technologies</p>
+            </div>
+            <div>
+              <h6 className='text-5xl font-medium mb-3'>7+</h6>
+              <p className='text-xl'>Seniority levels</p>
+            </div>
+          </div>
+          <div className='mt-16'>
+            <Button size='large'>Hire Developers</Button>
+            <Button size='large' className='ml-4'>
+              Apply for Jobs
+            </Button>
+          </div>
+        </div>
+        <div className='absolute top-16 right-32 z-0'>
+          <img src={world} alt='world' />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WorldStats;
