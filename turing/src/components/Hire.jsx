110cd94db33d83c34ef7a57ba99243a11b936c79
tinhtt <tinhtrantrung.vn@gmail.com>
2023-11-28 20:05:28 +1300

    Turing - implement Hire component.

--- turing/src/components/Hire.jsx
+++ turing/src/components/Hire.jsx
@@ -1,7 +1,17 @@
+import Button from './Button';
+
 const Hire = () => {
   return (
-    <section className='py-16'>
-      <div className='container'></div>
+    <section className='py-16 bg-black text-white'>
+      <div className='container flex flex-col items-center'>
+        <h4 className='text-4xl/[51px] font-medium mb-6'>Hire remote developers</h4>
+        <p className='mb-8 text-center'>
+          Tell us the skills you need and we&rsquo;ll find the best developer for you in days,
+          <br />
+          not weeks.
+        </p>
+        <Button>Hire Developers</Button>
+      </div>
     </section>
   );
 };
