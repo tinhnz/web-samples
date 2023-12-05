0828c130fcfe250b2411a41952ce3fd1837db8fa
tinhtt <tinhtrantrung.vn@gmail.com>
2023-12-05 18:51:23 +1300

    Hoobank - Add code.

---
+++ hoobank/README.md
@@ -0,0 +1,13 @@
+# React + Vite
+https://youtu.be/_oO4Qi5aVZs?si=nFb7R4ySELXhtOze
+
+Install tailwind CSS
+- https://tailwindcss.com/docs/guides/vite
+- Copy & paste `tailwind.config.js`, `index.css` from author repo.
+
+Constants and Assets (src/constants/index.js, src/assets, src/style.js)
+- Download from video descriptions, extract them into this project.
+
+Create all required components.
+Build and Deploy
+- npm run build
---
+++ hoobank/src/components/CardDeal.jsx
@@ -0,0 +1,23 @@
+import { card } from '../assets';
+import styles, { layout } from '../style';
+import Button from './Button';
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className='sm:block hidden' /> in few easy steps.
+      </h2>
+      <p className={`${styles.paragraph} mt-5 max-w-[470px]`}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi, perspiciatis voluptas.
+      </p>
+      <Button styles='mt-10' />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
